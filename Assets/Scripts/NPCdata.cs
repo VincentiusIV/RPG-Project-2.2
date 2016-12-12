5b7b6697dec584da79bbb3a8ac8a00d7fedf1ac8
Vincent Versnel <vincent@versnelhifi.nl>
2016-12-12 11:28:29 +0100

    added trigger zone with basic functionality for NPC's.

--- Assets/Scripts/NPCdata.cs
+++ Assets/Scripts/NPCdata.cs
@@ -3,13 +3,41 @@ using System.Collections;
 
 public class NPCdata : MonoBehaviour
 {
-    [HideInInspector]public Item thisNPC;
+    public Item thisNPC = new Item();
+    public int id;
     private DatabaseHandler db;
+    private GameObject notification;
 
-	void Awake ()
+    void Start()
     {
-	    db = GameObject.FindWithTag("Inventory").GetComponent<DatabaseHandler>();
+        notification = transform.FindChild("HelpText").gameObject;
+        notification.SetActive(false);
+        db = GameObject.FindWithTag("Inventory").GetComponent<DatabaseHandler>();
+        thisNPC = db.FetchItemByID(id);
+
+        Sprite spriteData = Resources.Load<Sprite>("Sprites/" + thisNPC.Type + "/" + thisNPC.Slug);
     }
 	
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("Collided with " + col.gameObject.name);
+        notification.SetActive(true);
+    }
 
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if(Input.GetButtonDown("Interact"))
+        {
+            Debug.Log("You interacted with NPC: " + thisNPC.Title);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        notification.SetActive(false);
+    }
+    public void OpenTrade()
+    {
+
+    }
 }
