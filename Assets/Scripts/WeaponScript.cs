166e39320de7a817b50aa6c9e1d0d8014fe5b2ed
Vincent Versnel <vincent@versnelhifi.nl>
2016-12-19 13:27:19 +0100

    Created WeaponScript, removed Merchant script

---
+++ Assets/Scripts/WeaponScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponScript : MonoBehaviour
+{
+    private string wepName;
+    private SpriteRenderer sr;
+
+	void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    public void MeleeAttack(int dmg, int range)
+    {
+        // modify collider and do dmg to 
+    }
+
+    public void RangedAttack(int dmg, int range, GameObject proj)
+    {
+        Instantiate(proj, );
+    }
+
+    public void SpecialAttack()
+    {
+        // Something special
+    }
+}
