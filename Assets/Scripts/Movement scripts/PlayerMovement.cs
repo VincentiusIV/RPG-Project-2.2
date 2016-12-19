1026a86e505156fe7e52ce921022cc92850933bc
Vincent Versnel <vincent@versnelhifi.nl>
2016-12-19 13:00:48 +0100

    Moved PlayerStats to seperate script

--- Assets/Scripts/Movement scripts/PlayerMovement.cs
+++ Assets/Scripts/Movement scripts/PlayerMovement.cs
@@ -118,16 +118,3 @@ public class PlayerMovement : MonoBehaviour
     }*/
 }
 
-public class PlayerStats
-{
-    int Power { get; set; }
-    int Defence { get; set; }
-    int HP { get; set; }
-
-    public PlayerStats(int pow, int def, int hp)
-    {
-        this.Power = pow;
-        this.Defence = def;
-        this.HP = hp;
-    }
-}
---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStats
+{
+    int Power { get; set; }
+    int Defence { get; set; }
+    int HP { get; set; }
+
+    public PlayerStats(int pow, int def, int hp)
+    {
+        this.Power = pow;
+        this.Defence = def;
+        this.HP = hp;
+    }
+}
+
