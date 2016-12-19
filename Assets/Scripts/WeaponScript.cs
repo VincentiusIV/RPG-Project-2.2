fb86b9b7a7d536f9034a076c8bd9cfd0cb998a0b
Vincent Versnel <vincent@versnelhifi.nl>
2016-12-19 13:43:22 +0100

    wrote thought process in comments on equipment system, updated new player prefab, organized assets folder a little

--- Assets/Scripts/WeaponScript.cs
+++ Assets/Scripts/WeaponScript.cs
@@ -4,21 +4,33 @@ using System.Collections;
 public class WeaponScript : MonoBehaviour
 {
     private string wepName;
-    private SpriteRenderer sr;
+    private SpriteRenderer sr; // change this to animator in the future
+
+    private PlayerStats ps; // maybe not needed if dmg is calculated beforehand
 
 	void Start()
     {
         sr = GetComponent<SpriteRenderer>();
+
+        // ps = GetComponent
     }
 
     public void MeleeAttack(int dmg, int range)
     {
-        // modify collider and do dmg to 
+        /* Modify collider radius with range
+         * change damage based on player stats
+         * Do dmg to rigidbodies with the right tags inside own collider
+         * 
+         * */
     }
 
-    public void RangedAttack(int dmg, int range, GameObject proj)
+    public void RangedAttack(int dmg, int range, GameObject proj, float projSpeed)
     {
-        Instantiate(proj, );
+        /* Instantiate(proj, );
+         * change damage based on player stats
+         * pass range to projectile script
+         * */
+
     }
 
     public void SpecialAttack()
