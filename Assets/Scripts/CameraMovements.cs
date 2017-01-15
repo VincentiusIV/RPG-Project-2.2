33d62c9e365ffd1acd4f5cc0a81e25026b9bffb7
Vincent Versnel <vincent@versnelhifi.nl>
2017-01-15 13:25:33 +0100

    updated cameramovement script to round off float value to nearest pixel, this reduces pixel edge flickering

--- Assets/Scripts/CameraMovements.cs
+++ Assets/Scripts/CameraMovements.cs
@@ -8,12 +8,18 @@ public class CameraMovements : MonoBehaviour
     public GameObject target;
     public float camDistance;
     public float lerpIntensity;
-
+    public float pixelToUnits;
     private Vector2 movement;
 
+    void Start()
+    {
+        GetComponent<Camera>().orthographicSize = (Screen.height / pixelToUnits) / 2;
+    }
     void FixedUpdate()
     {
-        FollowPlayer();
+        Vector3 targetPosition = new Vector3(RoundToNearestPixel(target.transform.position.x), RoundToNearestPixel(target.transform.position.y), 0f )+ new Vector3(0f, 0f, -camDistance);
+        
+        transform.position = Vector3.Lerp(transform.position, targetPosition, lerpIntensity);
     }
 
     void Update()
@@ -21,10 +27,11 @@ public class CameraMovements : MonoBehaviour
         minimap.transform.position = new Vector3(target.transform.position.x, target.transform.position.y, minimap.transform.position.z);
     }
 
-    void FollowPlayer()
-    { 
-        Vector3 targetPosition = target.transform.position + new Vector3(0f, 0f, -camDistance);
-
-        transform.position = Vector3.Lerp(transform.position, targetPosition, lerpIntensity);
+    public float RoundToNearestPixel(float unityUnits)
+    {
+        float valueInPixels = unityUnits * pixelToUnits;
+        valueInPixels = Mathf.Round(valueInPixels);
+        float roundedUnityUnits = valueInPixels * (1 / pixelToUnits);
+        return roundedUnityUnits;
     }
 }
