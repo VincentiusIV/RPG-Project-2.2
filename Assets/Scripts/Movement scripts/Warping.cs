5952925ae8c35b5b40e0587e2b28477f23bf153d
Vincent Versnel <vincent@versnelhifi.nl>
2017-01-19 19:10:52 +0100

    implemented screen fading animations

--- Assets/Scripts/Movement scripts/Warping.cs
+++ Assets/Scripts/Movement scripts/Warping.cs
@@ -4,12 +4,22 @@ using System.Collections;
 public class Warping : MonoBehaviour {
 
     [SerializeField]private Transform targetWarp;
-	
-    void OnTriggerEnter2D(Collider2D col)
+    private ScreenFader sf;
+
+    void Start()
+    {
+        sf = GameObject.Find("ScreenFade").GetComponent<ScreenFader>();
+    }
+
+    IEnumerator OnTriggerEnter2D(Collider2D col)
     {
         if(col.CompareTag("Player"))
         {
+            yield return StartCoroutine(sf.FadeIn());
             col.transform.position = targetWarp.position;
+            Camera.main.transform.position = targetWarp.position;
+            yield return StartCoroutine(sf.FadeOut());
+            
         }
     }
 }
---
+++ Assets/Scripts/UI & Misc/ScreenFader.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenFader : MonoBehaviour
+{
+    private Animator ani;
+
+    void Start()
+    {
+        ani = GetComponent<Animator>();
+    }
+
+    public IEnumerator FadeIn()
+    {
+        Debug.Log("Fading in");
+        ani.SetBool("Fade", true);
+        yield return new WaitForSeconds(1f);
+    }
+    
+    public IEnumerator FadeOut()
+    {
+        Debug.Log("Fading out");
+        ani.SetBool("Fade", false);
+        yield return new WaitForSeconds(1f);
+    }         	
+}
