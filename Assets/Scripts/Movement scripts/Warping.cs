9246bbbc379cbb147a2192dccba3aa3c4419145f
Vincent Versnel <vincent@versnelhifi.nl>
2017-01-19 18:13:46 +0100

    implemented warping inside the same scene

---
+++ Assets/Scripts/Movement scripts/Warping.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Warping : MonoBehaviour {
+
+    [SerializeField]private Transform targetWarp;
+	
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.CompareTag("Player"))
+        {
+            col.transform.position = targetWarp.position;
+        }
+    }
+}
